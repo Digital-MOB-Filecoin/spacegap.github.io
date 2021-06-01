07b9ebd2e0a23d4ea036034728855241b4f5b003
Alexandru Matei <alexandrumatei3693@gmail.com>
2021-06-01 19:13:49 +0300

    Updates

---
+++ precache-manifest.7c5e276d5e16f88c164f0e457dd48daa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a2572288c85acb78ecb56478b56819",
+    "url": "/spacegap/index.html"
+  },
+  {
+    "revision": "3c334e937ddf8c65a4e6",
+    "url": "/spacegap/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "1b9e38a871fb46684744",
+    "url": "/spacegap/static/css/main.8d29f438.chunk.css"
+  },
+  {
+    "revision": "3c334e937ddf8c65a4e6",
+    "url": "/spacegap/static/js/2.df30ba99.chunk.js"
+  },
+  {
+    "revision": "9e872d9c3b16630e9299b8834fc56ac3",
+    "url": "/spacegap/static/js/2.df30ba99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9e38a871fb46684744",
+    "url": "/spacegap/static/js/main.c9b1a538.chunk.js"
+  },
+  {
+    "revision": "db5d4dfe092a0e2518d3",
+    "url": "/spacegap/static/js/runtime-main.d8078e19.js"
+  }
+]);
